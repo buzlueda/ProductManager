17e5960d2db584455859dd6021a5c29c19a62a08
buzlueda <edabuzlu@gmail.com>
2023-05-03 11:45:05 +0300

    Product manager&service created

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Entities.Concrete;
 
-namespace Business.Abstract
+namespace Business.Abstract;
+public interface IProductService
 {
-    public interface IProductService
-    {
-        
-    }
+    List<Product> GetAll();
+    Product GetById();
+    void Add(Product product);
+    void Update(Product product);
+    void Delete(Product product);
+
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,12 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
 
-namespace Business.Concrete
+namespace Business.Concrete;
+public class ProductManager : IProductService
 {
-    public interface ProductManager
+    IProductDal _productDal;
+    public ProductManager(IProductDal productDal)
     {
-        
+        _productDal = productDal;
+    }
+    public void Add(Product product)
+    {
+
+    }
+
+    public void Delete(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Product> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product GetById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Product product)
+    {
+        throw new NotImplementedException();
     }
 }
